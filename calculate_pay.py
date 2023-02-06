4f3447813f099e0a03d5fd98ac9ca9ce4a8dea44
Nikita <nikita.olechko@gmail.com>
2023-02-06 11:06:12 -0800

    Fix base_wages variable

--- calculate_pay.py
+++ calculate_pay.py
@@ -2,7 +2,7 @@ def calculate_pay(hours, wage):
     if hours <= 0 or wage <= 0:
         return 0
     if hours > 40:
-        base_wages = hours * wage
+        base_wages = 40 * wage
         overtime = (hours - 40) * (2 * wage)
         total_wages = base_wages + overtime
     else:
